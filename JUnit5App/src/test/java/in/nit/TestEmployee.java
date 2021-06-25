256c4f3654a30886456831b761f3c189d4a24587
vishnuvardhan621 <vvardhan621@gmail.com>
2021-06-25 23:09:24 +0530

    ..

---
+++ JUnit5App/src/test/java/in/nit/TestEmployee.java
@@ -0,0 +1,19 @@
+package in.nit;
+
+import org.junit.jupiter.api.Test;
+
+public class TestEmployee {
+
+	@Test
+	public void testSave() {
+		System.out.println("HELLO");
+	}
+	@Test
+	public void testUpdate() {
+		System.out.println("HELLO-UPDATE");
+	}
+	@Test
+	public void testDelete() {
+		System.out.println("HELLO-DELETE");
+	}
+}
